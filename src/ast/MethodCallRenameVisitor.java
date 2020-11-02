6f860ace57d86165a47036097701198acc8f3747
adargut <adar276@gmail.com>
2020-11-02 22:20:07 +0200

    some progress on MethodCallRenameVisitor

---
+++ src/ast/MethodCallRenameVisitor.java
@@ -0,0 +1,212 @@
+package ast;
+
+import utils.Mapper;
+import utils.VariablesMapper;
+import java.util.Set;
+
+public class MethodCallRenameVisitor implements Visitor {
+    private final String newName;
+    private final Set<String> relevantClasses; // todo get this from somewhere
+    private Mapper variableMapper = new VariablesMapper();
+    private ClassDecl currentClass;
+
+    public MethodCallRenameVisitor(String newName, Set<String> relevantClasses) {
+        this.newName = newName;
+        this.relevantClasses = relevantClasses;
+    }
+
+    @Override
+    public void visit(Program program) {
+        variableMapper.resetMapping();
+        for (var classdecl : program.classDecls()) {
+            classdecl.accept(this);
+        }
+    }
+
+    @Override
+    public void visit(ClassDecl classDecl) {
+        currentClass = classDecl;
+        for (var methodDecl : classDecl.methoddecls()) {
+            methodDecl.accept(this);
+        }
+    }
+
+    @Override
+    public void visit(MainClass mainClass) {
+        mainClass.mainStatement().accept(this);
+    }
+
+    @Override
+    public void visit(MethodDecl methodDecl) {
+
+    }
+
+    @Override
+    public void visit(FormalArg formalArg) {
+
+    }
+
+    @Override
+    public void visit(VarDecl varDecl) {
+        // todo check if this should be tostring?
+        variableMapper.createMapping(varDecl.name(), varDecl.type().toString());
+    }
+
+    @Override
+    public void visit(BlockStatement blockStatement) {
+        for (var statement : blockStatement.statements()) {
+            statement.accept(this);
+        }
+    }
+
+    @Override
+    public void visit(IfStatement ifStatement) {
+        ifStatement.cond().accept(this);
+        ifStatement.thencase().accept(this);
+        ifStatement.elsecase().accept(this);
+    }
+
+    @Override
+    public void visit(WhileStatement whileStatement) {
+        whileStatement.cond().accept(this);
+        whileStatement.body().accept(this);
+    }
+
+    @Override
+    public void visit(SysoutStatement sysoutStatement) {
+        sysoutStatement.arg().accept(this);
+    }
+
+    @Override
+    public void visit(AssignStatement assignStatement) {
+        assignStatement.rv().accept(this);
+    }
+
+    @Override
+    public void visit(AssignArrayStatement assignArrayStatement) {
+        assignArrayStatement.index().accept(this);
+        assignArrayStatement.rv().accept(this);
+    }
+
+    @Override
+    public void visit(AndExpr e) {
+        e.e1().accept(this);
+        e.e2().accept(this);
+    }
+
+    @Override
+    public void visit(LtExpr e) {
+        e.e1().accept(this);
+        e.e2().accept(this);
+    }
+
+    @Override
+    public void visit(AddExpr e) {
+        e.e1().accept(this);
+        e.e2().accept(this);
+    }
+
+    @Override
+    public void visit(SubtractExpr e) {
+        e.e1().accept(this);
+        e.e2().accept(this);
+    }
+
+    @Override
+    public void visit(MultExpr e) {
+        e.e1().accept(this);
+        e.e2().accept(this);
+    }
+
+    @Override
+    public void visit(ArrayAccessExpr e) {
+        e.indexExpr().accept(this);
+        e.arrayExpr().accept(this); // todo is this needed?
+    }
+
+    @Override
+    public void visit(ArrayLengthExpr e) {
+        e.arrayExpr().accept(this);
+    }
+
+    @Override
+    public void visit(MethodCallExpr e) {
+        // todo change this at the end to change after traversal
+        var owner = e.ownerExpr();
+
+        // Case this.foo()
+        if (e.ownerExpr() instanceof ThisExpr && relevantClasses.contains(currentClass)) {
+            e.setMethodId(this.newName);
+        }
+
+        // Case x.foo()
+        if (owner instanceof IdentifierExpr && relevantClasses.contains(((IdentifierExpr) owner).id())) {
+            e.setMethodId(this.newName);
+        }
+
+        // Case New X().foo()
+        if (owner instanceof NewObjectExpr && relevantClasses.contains(((NewObjectExpr) owner).classId()) {
+            e.setMethodId(this.newName);
+        }
+    }
+
+    @Override
+    public void visit(IntegerLiteralExpr e) {
+
+    }
+
+    @Override
+    public void visit(TrueExpr e) {
+
+    }
+
+    @Override
+    public void visit(FalseExpr e) {
+
+    }
+
+    @Override
+    public void visit(IdentifierExpr e) {
+
+    }
+
+    @Override
+    public void visit(ThisExpr e) {
+
+    }
+
+    @Override
+    public void visit(NewIntArrayExpr e) {
+
+    }
+
+    @Override
+    public void visit(NewObjectExpr e) {
+
+    }
+
+    @Override
+    public void visit(NotExpr e) {
+
+    }
+
+    @Override
+    public void visit(IntAstType t) {
+
+    }
+
+    @Override
+    public void visit(BoolAstType t) {
+
+    }
+
+    @Override
+    public void visit(IntArrayAstType t) {
+
+    }
+
+    @Override
+    public void visit(RefType t) {
+
+    }
+}
