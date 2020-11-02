0672164fa31f6a9e8ef0dd90afa204671f740606
adargut <adar276@gmail.com>
2020-11-03 00:19:41 +0200

    now utils are here too

--- src/utils/Mapper.java
+++ src/utils/Mapper.java
@@ -1,2 +1,30 @@
-package utils;public interface Mapper {
+package utils;
+
+import ast.AstNode;
+import ast.AstType;
+import java.util.List;
+import java.util.Map;
+
+public interface Mapper {
+    /**
+     * Reset all previous mapping
+     *
+     */
+    public void resetMapping();
+
+    /**
+     * Return the type of the variable at given line.
+     *
+     * @param varName - name of the variable of interest.
+     * @return type of variable. // todo check if static or dynamic:
+     */
+    public String getMapping(String varName);
+
+    /**
+     * Create a new mapping for an integer and a line.
+     *
+     * @param varName - name of the variable of interest.
+     * @param varType - the type of the variable
+     */
+    public void createMapping(String varName, String varType);
 }
--- src/utils/VariablesMapper.java
+++ src/utils/VariablesMapper.java
@@ -1,2 +1,38 @@
-package utils;public class VariablesMapper {
+package utils;
+
+import java.util.Map;
+import ast.AstNode;
+
+public class VariablesMapper implements Mapper {
+    Map<String, String> variableMap;
+
+    /**
+     * @{inheritDoc}
+     *
+     */
+    @Override
+    public void resetMapping() {
+        variableMap.clear();
+    }
+
+    /**
+     * @{inheritDoc}
+     * Implemented using binary search on the list associated with the given variable.
+     *
+     */
+    @Override
+    public String getMapping(String varName) {
+        return variableMap.get(varName);
+    }
+
+    /**
+     * @{inheritDoc}
+     * Implemented by mapping a variable to a list of <Integer, String> pairs denoting the type it has at given line.
+     * For example: X x; Y y; x = y; would generate {x: [X, 1], [Y, 2], y: [Y, 1]}
+     *
+     */
+    @Override
+    public void createMapping(String varName, String varType) {
+        variableMap.put(varName, varType);
+    }
 }
